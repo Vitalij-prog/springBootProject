11defdf0a1ff982d30bd8d70589ad8757ed17e63
Vitalij Bogdevich <vitalij_bogdevich_prog@mail.ru>
2021-02-24 17:50:10 +0300

    initial commit

---
+++ src/main/java/org/vital/com/Product.java
@@ -0,0 +1,70 @@
+package org.vital.com;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "madein")
+    private String madeIn;
+
+    @Column(name = "product_amount")
+    private int amount;
+
+    @Column(name = "product_price")
+    private double price;
+
+    protected Product() {
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMadeIn() {
+        return madeIn;
+    }
+
+    public void setMadeIn(String madeIn) {
+        this.madeIn = madeIn;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/org/vital/com/ProductService.java
@@ -0,0 +1,31 @@
+package org.vital.com;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repo;
+
+    public List<Product> listAll() {
+        return repo.findAll();
+    }
+
+    public void save(Product product) {
+        repo.save(product);
+    }
+
+    public Product get(int id) {
+        return repo.findById(id).get();
+    }
+
+    public void delete(int id) {
+        repo.deleteById(id);
+    }
+}
